772d678b81958db7280cbb9da1bae9c839ae2d49
SharoonNasimNJ <sharoon493@gmail.com>
2021-11-18 20:57:40 +0500

    Main.py updated

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 def work(a):
     b = a+5
-    sum = 0
-    for i in range(b):
-        for j in range(a):
-            sum+=i*j
-        sum+=i*sum
-    return sum
+    return b
 
 print(work(4))
 print("Sharoon Nasim Hello World!")
